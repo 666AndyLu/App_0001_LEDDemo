bdd4dce279591385d13e94e45c343131f8b56314
666AndyLu <1131736298@qq.com>
2019-12-09 20:56:46 +0800

    v2, use jni

--- app/src/main/java/com/example/andylu/app_0001_leddemo/MainActivity.java
+++ app/src/main/java/com/example/andylu/app_0001_leddemo/MainActivity.java
@@ -28,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
                 checkboxled2.setChecked(true);
                 checkboxled3.setChecked(true);
                 checkboxled4.setChecked(true);
+
+                for(int i=0;i<4;i++)
+                {
+                    HardControl.ledCtrl(i,1);
+                }
             }
             else {
                 button.setText("ALL ON");
@@ -35,6 +40,11 @@ public class MainActivity extends AppCompatActivity {
                 checkboxled2.setChecked(false);
                 checkboxled3.setChecked(false);
                 checkboxled4.setChecked(false);
+
+                for(int i=0;i<4;i++)
+                {
+                    HardControl.ledCtrl(i,0);
+                }
             }
         }
     }
@@ -49,40 +59,48 @@ public class MainActivity extends AppCompatActivity {
                 if (checked) {
                     // Put some meat on the sandwich
                     Toast.makeText(getApplicationContext(), "LED1 on",Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(0, 1);
                 }
                 else{
                     // Remove the meat
                     Toast.makeText(getApplicationContext(), "LED1 off",Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(0, 0);
                 }
                 break;
             case R.id.LED2:
                 if (checked) {
                     // Put some meat on the sandwich
                     Toast.makeText(getApplicationContext(), "LED2 on",Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(1, 1);
                 }
                 else{
                     // Remove the meat
                     Toast.makeText(getApplicationContext(), "LED2 off",Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(1, 0);
                 }
                 break;
             case R.id.LED3:
                 if (checked) {
                     // Put some meat on the sandwich
                     Toast.makeText(getApplicationContext(), "LED3 on",Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(2, 1);
                 }
                 else{
                     // Remove the meat
                     Toast.makeText(getApplicationContext(), "LED3 off",Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(2, 0);
                 }
                 break;
             case R.id.LED4:
                 if (checked) {
                     // Put some meat on the sandwich
                     Toast.makeText(getApplicationContext(), "LED4 on",Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(3, 1);
                 }
                 else{
                     // Remove the meat
                     Toast.makeText(getApplicationContext(), "LED4 off",Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(3, 0);
                 }
                 break;
             // TODO: Veggie sandwich
@@ -94,6 +112,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HardControl.ledOpen();
 
         button = (Button) findViewById(R.id.BUTTON);
         checkboxled1 = (CheckBox) findViewById(R.id.LED1);        checkboxled1 = (CheckBox) findViewById(R.id.LED1);
